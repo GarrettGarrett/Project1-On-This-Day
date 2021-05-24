0fec5d5e36a94f2a27f8f6c659247de49ce899c8
PyPy <pythonautomatefinance@gmail.com>
2021-05-24 07:29:38 -0700

    organize API info into 3 lists

--- js/script.js
+++ js/script.js
@@ -1,21 +1,43 @@
 /*----- constants -----*/
 let date = "5/21"
+let eventList = []
+let deathsList = [] 
+let birthsList = []
+
 /*----- app's state (variables) -----*/
-let apiData;
+let apiInfo;
+
 /*----- cached element references -----*/
+
 /*----- event listeners -----*/
+
 /*----- functions -----*/
 function getApiData () {
         $.ajax(`http://history.muffinlabs.com/date/${date}`)
     .then(function(data) { 
-        apiData = data;
+        apiInfo = data;
         render();
         });
 }
 
+
 function render() {
-        console.log(apiData.data.Events);
-};   
+        eventList.length = 0 //clear the evenList
+        deathsList.length = 0 //clear the deathsList
+        birthsList.length = 0 //clear the birthsList
+        for (let i = 0; i < apiInfo.data.Events.length; i++) {
+                eventList.push(apiInfo.data.Events[i].html)
+
+                deathsList.push(apiInfo.data.Deaths[i].html)
+
+                birthsList.push(apiInfo.data.Births[i].html)
+              }
+}; 
 
 
-getApiData()
+getApiData();
+console.log('events', eventList)
+console.log('births', birthsList)
+console.log('deaths', deathsList)
+
+
